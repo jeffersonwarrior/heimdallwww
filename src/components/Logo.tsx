05d377b547d24175ea2ad6fffdb7b3c52bf8bac3
jeffersonwarrior <jefferson@jeffersonnunn.com>
2025-09-17 11:15:01 -0500

    [dyad] Updated logo to external URL - wrote 1 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     <a href="/" className="flex items-center">
       <img
-        src="/heimdall-logo.svg" // Updated to use the local SVG file
+        src="https://static.wixstatic.com/media/edf5ac_fbd61cdd8106430baaa6de27e6cd9aa6~mv2.png/v1/crop/x_272,y_304,w_1251,h_592/fill/w_470,h_222,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Heimdall%20(1).png" // Updated to use the provided external PNG URL
         alt="Heimdall Strategy Logo"
         className="h-8 w-auto"
       />
