a9051652d65e5150022b9ac9de27869a3842c881
jeffersonwarrior <jefferson@jeffersonnunn.com>
2025-09-17 11:02:43 -0500

    [dyad] Updated logo to use image from website - wrote 1 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import React from 'react';
-import { ShieldCheck } from 'lucide-react';
 
 const Logo = () => {
   return (
-    <a href="/" className="flex items-center space-x-2">
-      <ShieldCheck className="h-8 w-auto text-heimdall-primary-navy" />
-      <span className="text-xl font-bold text-heimdall-primary-navy">Heimdall Strategy</span>
+    <a href="/" className="flex items-center">
+      <img
+        src="https://www.heimdallstrategy.com/logo.svg"
+        alt="Heimdall Strategy Logo"
+        className="h-8 w-auto"
+      />
     </a>
   );
 };
