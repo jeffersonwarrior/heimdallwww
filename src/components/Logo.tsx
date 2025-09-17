05809bf434a2f6e3fbee59136fb8158041a16b2b
jeffersonwarrior <jefferson@jeffersonnunn.com>
2025-09-17 11:15:50 -0500

    [dyad] Adjusted logo size - wrote 1 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,9 +6,9 @@ const Logo = () => {
   return (
     <a href="/" className="flex items-center">
       <img
-        src="https://static.wixstatic.com/media/edf5ac_fbd61cdd8106430baaa6de27e6cd9aa6~mv2.png/v1/crop/x_272,y_304,w_1251,h_592/fill/w_470,h_222,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Heimdall%20(1).png" // Updated to use the provided external PNG URL
+        src="https://static.wixstatic.com/media/edf5ac_fbd61cdd8106430baaa6de27e6cd9aa6~mv2.png/v1/crop/x_272,y_304,w_1251,h_592/fill/w_470,h_222,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Heimdall%20(1).png"
         alt="Heimdall Strategy Logo"
-        className="h-8 w-auto"
+        className="h-10 w-auto"
       />
     </a>
   );
